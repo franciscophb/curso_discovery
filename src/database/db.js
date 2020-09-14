a90567b37862e1bc96930f17dcaebf0d64f8e86c
Francisco Araujo <franciscophb@hotmail.com>
2020-09-14 05:38:18 -0300

    test sqlite

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,8 @@
 const Database = require('sqlite-async');
-Database.open(__dirname + '/database.sqlite').then(execute)
 
 function execute(db){
     //criar as tabelas do banco de dados
-    db.exec(
+    return db.exec('
         CREATE TABLE IF NOT EXISTS proffys (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
@@ -25,5 +24,8 @@ function execute(db){
             time_from INTEGER,
             time_to INTEGER
         );
-    )
+    
+    ')
 }
+
+module.exports = Database.open(__dirname + '/database.sqlite').then(execute)
---
+++ src/database/test.js
@@ -0,0 +1,16 @@
+const Database = require('./db')
+
+Database.then((db) => {
+    //inserir dados
+    proffy = {
+        name:"Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "899876545434", 
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br/>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+       
+    }
+
+
+    //consultar os dados
+
+})
